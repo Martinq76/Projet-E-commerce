46049b93af7e82f6c53a4dbe954ea331082becf8
Pokmand <manudesj@msn.com>
2019-04-24 13:51:41 +0200

    affichage dynamique des rubriques ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,44 @@
+// Affichage Page de démarrage :
+$(".containerPC2").hide();
+$(".containerMobile2").hide();
+$(".containerAccessories2").hide();
+
 $(function(){
-  $(#desktopPc).click(function(){
-    $(#)
-  }
+// Affichage des catégories :
+// Nos produits :
+$("#AllProducts").click(function(){
+      $(".containerPC").hide();
+      $(".containerMobile").hide();
+      $(".containerAccessories").hide();
+      $(".containerPC2").show();
+      $(".containerMobile2").show();
+      $(".containerAccessories2").show();
+    })
+// Pc de Bureau :
+$("#desktopPc").click(function(){
+      $(".containerPC").hide();
+      $(".containerPC2").show();
+      $(".containerMobile").hide();
+      $(".containerMobile2").hide();
+      $(".containerAccessories").hide();
+      $(".containerAccessories2").hide();
+    })
+// Mobile :
+$("#mobile").click(function(){
+      $(".containerPC").hide();
+      $(".containerPC2").hide();
+      $(".containerMobile").hide();
+      $(".containerMobile2").show();
+      $(".containerAccessories").hide();
+      $(".containerAccessories2").hide();
+    })
+// Accessoires :
+    $("#accessories").click(function(){
+          $(".containerPC").hide();
+          $(".containerPC2").hide();
+          $(".containerMobile").hide();
+          $(".containerMobile2").hide();
+          $(".containerAccessories").hide();
+          $(".containerAccessories2").show();
+        })
 })
