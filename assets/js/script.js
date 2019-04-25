149da8ef4d572b72acb5d215cf75c897c5465cba
Pokmand <manudesj@msn.com>
2019-04-26 01:44:24 +0200

    meme avec le cul ça marche pas

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,10 @@ $(function() {
     $(".containerAccessories").hide();
     $(".containerAccessories2").show();
   })
+//   $(".imgpc").mouseover(function() {
+//     var newid = $(this).id;
+//     console.log(idcible);
+// });
 
   // Mise en animation des descriptifs :
   $("#image1").mouseover(function() {
@@ -379,7 +383,7 @@ $(function() {
   $("#image48").mouseout(function() {
     $("#description48").hide();
   })
-})
+
 //Fin affichage catégories
 
 
@@ -389,50 +393,57 @@ var price = ['549.95', '2549.95', '679,94', '3299,95', '599,95', '1079.95', '164
 
 var nameProducts = ['ASUS A41GAT', 'Coffee Macchiato', 'Chopin', 'ASUS ROG G12CM', 'NUC-I5-8-S1', 'Bazooka', 'Macchiato Artic Refresh', 'Lenovo V330-20ICB AIO', 'Lenovo ThinkStation P320 Tiny', 'MSI Pro 16 Flex 8GL-001XEU', 'MSI Trident 3 VR7RC', 'MSI Vortex W25 8SM-098FR', 'IPhone Xs Max', 'Samsung Galaxy S8', 'Honor 9 Lite', 'Huawei Mate 20 Pro', 'Honor 10 lite', 'Huawey p 30', 'Xiaomi redmi 7', 'Samsung A50', 'Iphone 7', 'Blackberry KEYone', 'ASUS ROG Phone ZS600KL', 'Motorola Moto G7 Play', 'G.Skill Aegis (2 x 8 Go) DDR4', 'NVIDIA TITAN RTX', 'ASUS ROG STRIX Z390-E', 'Seagate BarraCuda 2 To', 'Antec HCG750', 'ASUS BC-12D2HT', 'Aerocool ORBIT RC', 'Kit Upgrade AMD Ryzen ASUS PRIME', 'Intel core I9', 'ASUS Strix Soar', 'Noctua NH-D15 SE-AM4', 'Samsung SSD 860 EVO 500 Go'];
 
+$('.addButton').click(function() {
+  var index = $(this).val();
+  console.log(index);
+  var tableau = '<tr><td>' + nameProducts[index] + '</td><td>' + code[index] + '</td><td>' + price[index] + '€' + '</td><td>' + '<input type="text" id="quantityCaddy" value="1" />' + '</td><td>' + '</td></tr>';
+  $('#tableau > tbody:last').append(tableau);
+
+$(document).keyup(function(){
+  var quantityproduct = $("#quantityCaddy").val();
+  console.log(quantityproduct);
+  var subtotalprice = quantityproduct * price[index];
+  console.log(subtotalprice);
+  $('tbody>tr').empty();
+var tableau = '<tr><td>' + nameProducts[index] + '</td><td>' + code[index] + '</td><td>' + price[index] + '€' + '</td><td>' + '<input type="text" id="quantityCaddy" />' + '</td><td>' + subtotalprice + '</td></tr>';
+$('#tableau > tbody:last').append(tableau);
+});
+});
+})
+
+
+
 // $('.addButton').click(function() {
 //   var index = $(this).val();
 //   console.log(index);
-//   var tableau = '<tr><td>' + nameProducts[index] + '</td><td>' + code[index] + '</td><td>' + price[index] + '€' + '</td><td>' + '<input type="number" id="quantityCaddy" value="1" />' + '</td></tr>';
+//   var tableau = '<tr><td>' + nameProducts[index] + '</td><td>' + code[index] + '</td><td>' + price[index] + '€' + '</td><td>' + '<button id="buttonMoins"> - </button>' + '<input type="number" id="quantityCaddy" value="1" />' + '<button id="buttonPlus"> + </button>' + '</td></tr>';
 //   $('#tableau > tbody:last').append(tableau);
 //   $('#priceCaddy').text(price[index]);
 //   var quantityCaddyup = 'quantityCaddy' + [index];
 //   console.log(quantityCaddyup);
 //   $('.addButton').prop('id', 'quantityCaddyup');
-//   var ma_variable = $("#quantityCaddyup").val();
-//   console.log("total " + ma_variable);
-
-$('.addButton').click(function() {
-  var index = $(this).val();
-  console.log(index);
-  var tableau = '<tr><td>' + nameProducts[index] + '</td><td>' + code[index] + '</td><td>' + price[index] + '€' + '</td><td>' + '<button id="buttonMoins"> - </button>' + '<input type="number" id="quantityCaddy" value="1" />' + '<button id="buttonPlus"> + </button>' + '</td></tr>';
-  $('#tableau > tbody:last').append(tableau);
-  $('#priceCaddy').text(price[index]);
-  var quantityCaddyup = 'quantityCaddy' + [index];
-  console.log(quantityCaddyup);
-  $('.addButton').prop('id', 'quantityCaddyup');
-
-$('#buttonMoins').click(function() {
-  var quantityCaddy = $('#quantityCaddyup').val();
-  console.log('QTY : ' + $('#quantityCaddyup').val());
-
-  if (quantityCaddy <= 1) {
-    $('tbody>tr').empty();
-  } else {
-    quantityCaddy--;
-    $('#quantityCaddyup').val(quantityCaddy);
-    console.log(quantityCaddy);
-  }
-});
-
-$('#buttonPlus').click(function() {
-  var quantityCaddy = $('#quantityCaddy').val();
-  quantityCaddy++;
-  $('#quantityCaddy').val(quantityCaddy);
-  console.log('QTY : ' + $('#quantityCaddy'));
-});
-
-console.log(quantityCaddy);
-console.log(price[0]);
-console.log(code[0]);
-console.log(nameProducts[0]);
-});
+//
+// $('#buttonMoins').click(function() {
+//   var quantityCaddy = $('#quantityCaddyup').val();
+//   console.log('QTY : ' + $('#quantityCaddyup').val());
+//
+//   if (quantityCaddy <= 1) {
+//     $('tbody>tr').empty();
+//   } else {
+//     quantityCaddy--;
+//     $('#quantityCaddyup').val(quantityCaddy);
+//     console.log(quantityCaddy);
+//   }
+// });
+//
+// $('#buttonPlus').click(function() {
+//   var quantityCaddy = $('#quantityCaddy').val();
+//   quantityCaddy++;
+//   $('#quantityCaddy').val(quantityCaddy);
+//   console.log('QTY : ' + $('#quantityCaddy'));
+// });
+//
+// console.log(quantityCaddy);
+// console.log(price[0]);
+// console.log(code[0]);
+// console.log(nameProducts[0]);
